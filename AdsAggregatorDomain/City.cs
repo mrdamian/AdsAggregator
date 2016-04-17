6484b361638d2191b7f8e6eb17626cac32e7adc6
mrdamian <mrdamian.db@gmail.com>
2016-04-17 17:52:52 +0300

    544377 - Added projects for domain models and data access.

---
+++ AdsAggregatorDomain/City.cs
@@ -0,0 +1,40 @@
+﻿namespace AdsAggregatorDomain
+{
+    public class City
+    {
+        private int _cityId;
+        private string _name;
+
+        public City(int cityId, string name)
+        {
+            _cityId = cityId;
+            _name = name;
+        }
+
+        public int CityId
+        {
+            get
+            {
+                return _cityId;
+            }
+
+            set
+            {
+                _cityId = value;
+            }
+        }
+
+        public string Name1
+        {
+            get
+            {
+                return _name;
+            }
+
+            set
+            {
+                _name = value;
+            }
+        }
+    }
+}
---
+++ AdsAggregatorDomain/Street.cs
@@ -0,0 +1,55 @@
+﻿namespace AdsAggregatorDomain
+{
+    public class Street
+    {
+        private int _streetId;
+        private string _name;
+        private District _district;
+
+        public Street(int id, string name, District district)
+        {
+            _streetId = id;
+            _name = name;
+            _district = district;
+        }
+
+        public int StreetId
+        {
+            get
+            {
+                return _streetId;
+            }
+
+            set
+            {
+                _streetId = value;
+            }
+        }
+
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+
+            set
+            {
+                _name = value;
+            }
+        }
+
+        public District District
+        {
+            get
+            {
+                return _district;
+            }
+
+            set
+            {
+                _district = value;
+            }
+        }
+    }
+}
